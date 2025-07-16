a483f61530ee2bd025525ce0df6ab9f8626c5597
byhndo <120541041+byhndo@users.noreply.github.com>
2025-07-16 15:13:24 +0700

    Update and rename router.js to js/router.js

--- js/router.js
+++ js/router.js
@@ -2,7 +2,6 @@ const routes = [
   { path: '/', redirect: '/bio'},
   { name: "Bio", path: "/bio", component: Bio },
   { name: "Photos", path: "/photos", component: Photos },
-  { name: "Findme", path: "/findme", component: Findme },
   { path: "/:pathMatch(.*)*", redirect: "/" }
 ];
 
