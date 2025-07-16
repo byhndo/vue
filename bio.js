445c92247685a44daa25f13a155caf32cf046975
byhndo <120541041+byhndo@users.noreply.github.com>
2025-07-16 14:24:58 +0700

    Update and rename Tes to bio.js

---
+++ bio.js
@@ -0,0 +1,149 @@
+const Bio = {
+  template: `
+  <div> 
+
+
+<!-- sub1 -->
+<div class="sub1" key="bio" v-if="bg==='bio'">
+<div class="box1">
+<svg class="sub">
+<defs>
+<filter id='noise2' x='0%' y='0%' width='100%' height='100%'>
+<feGaussianBlur in="SourceGraphic" stdDeviation="0" result="blur"></feGaussianBlur>
+<feColorMatrix in="blur" mode="matrix" values="	1 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 18 -5" result="goo"></feColorMatrix>
+<feTurbulence type="fractalNoise" baseFrequency="0.07 0.3" numOctaves="1" seed="1" result="noise"></feTurbulence>
+<feDisplacementMap in="goo" in2="noise" scale="0" result="displacement"></feDisplacementMap>
+<feComposite in="SourceGraphic" in2="displacement" operator="atop"></feComposite>			 
+</defs>
+</filter> 
+</svg>
+<h3 class="one">Let Me Talk</h3>
+</div>
+<div class="vline"><div class="vl"></div></div>
+<div class="box2">
+<div class="two">
+<p class="sub1">
+<div class="prolog">Saya <span>Bayu Handono</span>, Saya lahir di Bandung, Jawa Barat, Indonesia, Asia Tenggara, Asia, Planet Bumi, Tata Surya, Bima Sakti, Jagat raya.</div>
+</p>
+</div>
+</div>
+<div class="box2">
+<div class="two">                                                    
+<p class="sub1">Selamat datang dan terima kasih sudah mau berkunjung ke sini. Semoga anda sehat dan bahagia selalu. Senantiasa semua makhluk berbahagia. Jangan minta motivasi, tidak ada kata-kata dan kalimat motivasi, karena saya bukan motivator. Motivasilah dirimu sendiri. Jangan pernah percaya dengan perkataan orang lain. Orang lain saja gak yakin dengan apa yang sedang dibicarakannya. Semua hal di alam semesta ini hanyalah asumsi dan dugaan belaka. Satu-satunya hal yang harus kamu percayai hanyalah mempercayai  dirimu sendiri. Khoeeeekh cuiihh&hellip;</p>
+</div>
+</div>
+<div class="box2">
+<div class="two">                                                    
+<p class="sub1">
+Saya tidak terlalu suka dengan kalimat&ndash;kalimat bijak orang lain, tapi setidaknya ada satu kalimat bagus yang saya suka
+<br>
+</p>
+</div>
+</div>
+<div class="box3">                                                 
+<p class="sub1">
+<div class="quote">
+&ldquo;Satu-satunya kebijaksanaan sejati 
+</div>
+<div class="quote">
+adalah mengetahui bahwa	Anda
+</div>
+<div class="quote">
+tidak tahu apa-apa.&rdquo;	
+</div>
+</p>
+</div>
+<div class="box3">
+<div class="cite">
+<div class="quote">
+~Socrates
+</div>
+</div>
+</div>
+<div class="rbox">
+<svg class="noise3">
+<defs>
+<filter id='noise3' x='0%' y='0%' width='100%' height='100%'>
+<feGaussianBlur in="SourceGraphic" stdDeviation="0" result="blur"></feGaussianBlur>
+<feColorMatrix in="blur" mode="matrix" values="	1 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 18 -5" result="goo"></feColorMatrix>
+<feTurbulence type="fractalNoise" baseFrequency="0.07 0.3" numOctaves="1" seed="1" result="noise"></feTurbulence>
+<feDisplacementMap in="goo" in2="noise" scale="0" result="displacement"></feDisplacementMap>
+<feComposite in="SourceGraphic" in2="displacement" operator="atop"></feComposite>			 
+</filter> 
+</defs>
+</svg>	
+<h4 class="one2">Tentang Saya</h4>				     
+</div>
+<div class="box">
+<div class="gr"></div>	
+</div>		    
+<ul>
+<div class="boxme"><div class="aboutme"><p class="sub1">	
+<li class="about">Percaya reinkarnasi</li>
+</p></div></div>
+<div class="boxline"><div class="line"></div></div>
+<div class="boxme"><div class="aboutme"><p class="sub1">	
+<li class="about">Percaya karma</li>
+</p></div></div>
+<div class="boxline"><div class="line"></div></div>
+<div class="boxme"><div class="aboutme"><p class="sub1">	
+<li class="about">Percaya adanya makhluk ekstraterestrial</li> 
+</p></div></div>
+<div class="boxline"><div class="line"></div></div>
+<div class="boxme"><div class="aboutme"><p class="sub1">	
+<li class="about">Sepertinya saya tidak pernah jatuh cinta(kayaknya sih). Sepertinya reaksi kimia semacam itu tidak ada dalam diri Saya</li>
+</p></div></div>
+<div class="boxline"><div class="line"></div></div>
+<div class="boxme"><div class="aboutme"><p class="sub1">	
+<li class="about">Suka DeathMetal, suka JKT48</li>	
+</p></div></div>
+<div class="boxline"><div class="line"></div></div>
+<div class="boxme"><div class="aboutme"><p class="sub1">	
+<li class="about">&ldquo;Apa agamamu? Agamaku adalah air yang membersihkan pertanyaanmu.&rdquo; <a href="https://www.google.com/search?q=joko+pinurbo&oq=joko+pinurbo&gs_lcrp=EgZjaHJvbWUqBwgAEAAYjwIyBwgAEAAYjwIyDQgBEC4YgwEYsQMYgAQyDQgCEAAYgwEYsQMYgAQyDQgDEAAYgwEYsQMYgAQyDQgEEAAYgwEYsQMYgAQyBggFEAAYAzIGCAYQABgDMg0IBxAAGIMBGLEDGIAEMg0ICBAAGIMBGLEDGIAEMgYICRAAGAMyDQgKEAAYgwEYsQMYgAQyBwgLEAAYgAQyBwgMEAAYgAQyDQgNEAAYgwEYsQMYgAQyBwgOEAAYgATSAQgyMzEzajBqN6gCFLACAQ&client=ms-android-samsung-ga-rev1&sourceid=chrome-mobile&ie=UTF-8" rel="noopener noreferer" target="_blank" title="Joko Pinurbo"><svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24"><path fill="#777" d="M8 13v-1h7v1zm7.5-6a5.5 5.5 0 0 1 5.5 5.5a5.5 5.5 0 0 1-5.5 5.5H13v-1h2.5c2.5 0 4.5-2 4.5-4.5S18 8 15.5 8H13V7zm-8 11A5.5 5.5 0 0 1 2 12.5A5.5 5.5 0 0 1 7.5 7H10v1H7.5C5 8 3 10 3 12.5S5 17 7.5 17H10v1z"/></svg></a> </li>
+</p></div></div>
+<div class="boxline"><div class="line"></div></div>
+<div class="boxme"><div class="aboutme"><p class="sub1">	
+<li class="about">Satu-satunya makhluk yang paling tidak pantas untuk dicintai.</li>	
+</p></div></div>
+<div class="boxline"><div class="line"></div></div>
+</ul>
+<div class="box2">
+<div class="two">
+<div class="b-text">
+	If it ain&rsquo;t broke, don&rsquo;t fix it</div>
+</div>
+</div>
+<!-- soc -->
+<div class="sc">
+<div class="item-soc">
+<a href="https://instagram.com/byhndo" rel="noopener noreferer">
+<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="var(--soc1)" d="M20.947 8.305a6.53 6.53 0 0 0-.419-2.216a4.61 4.61 0 0 0-2.633-2.633a6.606 6.606 0 0 0-2.186-.42c-.962-.043-1.267-.055-3.709-.055s-2.755 0-3.71.055a6.606 6.606 0 0 0-2.185.42a4.607 4.607 0 0 0-2.633 2.633a6.554 6.554 0 0 0-.419 2.185c-.043.963-.056 1.268-.056 3.71s0 2.754.056 3.71c.015.748.156 1.486.419 2.187a4.61 4.61 0 0 0 2.634 2.632a6.584 6.584 0 0 0 2.185.45c.963.043 1.268.056 3.71.056s2.755 0 3.71-.056a6.59 6.59 0 0 0 2.186-.419a4.615 4.615 0 0 0 2.633-2.633c.263-.7.404-1.438.419-2.187c.043-.962.056-1.267.056-3.71c-.002-2.442-.002-2.752-.058-3.709m-8.953 8.297c-2.554 0-4.623-2.069-4.623-4.623s2.069-4.623 4.623-4.623a4.623 4.623 0 0 1 0 9.246m4.807-8.339a1.077 1.077 0 0 1-1.078-1.078a1.077 1.077 0 1 1 2.155 0c0 .596-.482 1.078-1.077 1.078"/><circle cx="11.994" cy="11.979" r="3.003" fill="var(--soc1)"/></svg>
+</a>
+</div>
+<div class="item-soc">
+<a href="https://behance.net/byhndo" rel="noopener noreferer">
+<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="var(--soc1)" d="M7.803 5.731c.589 0 1.119.051 1.605.155c.483.103.895.273 1.243.508c.343.235.611.547.804.939c.187.387.28.871.28 1.443c0 .62-.14 1.138-.421 1.551c-.283.414-.7.753-1.256 1.015c.757.219 1.318.602 1.69 1.146c.374.549.557 1.206.557 1.976c0 .625-.119 1.162-.358 1.613a3.11 3.11 0 0 1-.974 1.114a4.315 4.315 0 0 1-1.399.64a6.287 6.287 0 0 1-1.609.206H2V5.731zm-.351 4.972c.48 0 .878-.114 1.192-.345c.312-.228.463-.604.463-1.119c0-.286-.051-.522-.151-.707a1.114 1.114 0 0 0-.417-.428a1.683 1.683 0 0 0-.597-.215a3.609 3.609 0 0 0-.697-.061H4.71v2.875zm.151 5.239c.267 0 .521-.023.76-.077c.241-.052.455-.136.637-.261c.182-.12.332-.283.44-.491c.109-.206.162-.475.162-.798c0-.634-.179-1.085-.533-1.358c-.355-.27-.831-.404-1.414-.404H4.71v3.39h2.893zm8.565-.041c.367.358.896.538 1.584.538c.493 0 .919-.125 1.278-.373c.354-.249.57-.515.653-.79h2.155c-.346 1.072-.871 1.838-1.589 2.299c-.709.463-1.572.693-2.58.693c-.702 0-1.334-.113-1.9-.337a4.033 4.033 0 0 1-1.439-.958a4.37 4.37 0 0 1-.905-1.485a5.433 5.433 0 0 1-.32-1.899c0-.666.111-1.289.329-1.864a4.376 4.376 0 0 1 .934-1.493c.405-.42.885-.751 1.444-.994a4.634 4.634 0 0 1 1.858-.362c.754 0 1.413.146 1.979.44a3.967 3.967 0 0 1 1.39 1.182c.363.493.622 1.058.783 1.691c.161.632.217 1.292.171 1.983h-6.431c.001.704.238 1.371.606 1.729m2.812-4.681c-.291-.322-.783-.496-1.385-.496c-.391 0-.714.065-.974.199a1.97 1.97 0 0 0-.62.491a1.772 1.772 0 0 0-.328.628a2.82 2.82 0 0 0-.111.587h3.982c-.058-.624-.272-1.085-.564-1.409m-3.918-4.663h4.989v1.215h-4.989z"/></svg>    
+</a>
+</div>
+</div>
+<!-- soc -->
+	
+<!-- footer -->
+<div class="box4">                           
+<div class="footer"><span>&copy; <span id="year"></span> &bull; Dibuat oleh <a href="/">Bayu Handono.</a></span>
+<script>const currentYear = new Date().getFullYear();
+document.getElementById("year").textContent = currentYear</script>
+</div>
+</div>
+<!-- footer -->
+	
+</div>
+<!-- sub1 -->
+
+
+
+
+
+    
+  </div> `
+};
