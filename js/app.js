a97d34a1d965ac3edaa5f2687bef28d46ecf5806
byhndo <120541041+byhndo@users.noreply.github.com>
2025-07-17 12:42:47 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -145,7 +145,7 @@ const { createRouter, createWebHistory } = VueRouter;
 		
 const app = Vue.createApp({  
     methods: {
-        afterEnter(el) {
+        onEnter(el) {
             setupReveal(el);
         },
         afterLeave(el) {
