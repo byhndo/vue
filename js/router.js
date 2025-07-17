c267e50af255f1eea1ec9aa02bca572e32781db2
byhndo <120541041+byhndo@users.noreply.github.com>
2025-07-17 11:33:20 +0700

    Update router.js

--- js/router.js
+++ js/router.js
@@ -9,3 +9,32 @@ const router = VueRouter.createRouter({
   history: VueRouter.createWebHashHistory(),
   routes
 });
+
+
+
+
+router.beforeEach((to, from, next) => {
+      document.getElementByClassName('preloader-wrap').classList.remove('hidden')
+      next()
+    })
+
+    router.afterEach(() => {
+      setTimeout(() => {
+        const imgs = [...document.querySelectorAll('img')]
+        const audios = [...document.querySelectorAll('audio')]
+
+        const imgPromises = imgs.map(img => new Promise(resolve => {
+          if (img.complete) return resolve()
+          img.onload = img.onerror = () => resolve()
+        }))
+
+        const audioPromises = audios.map(aud => new Promise(resolve => {
+          aud.oncanplaythrough = () => resolve()
+          aud.onerror = () => resolve()
+        }))
+
+        Promise.all([...imgPromises, ...audioPromises]).then(() => {
+          document.getElementById('preloader').classList.add('hidden')
+        })
+      }, 100)
+    })
