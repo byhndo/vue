eabd19cade74f00f8e750f7907dfb7b7c6b953aa
byhndo <120541041+byhndo@users.noreply.github.com>
2025-07-18 11:14:35 +0700

    Update router.js

--- js/router.js
+++ js/router.js
@@ -1,5 +1,5 @@
 const routes = [
-//  { path: '/', redirect: '/bio'},
+  { path: '/', redirect: '/bio'},
   { name: "Bio", path: "/bio", component: Bio },
   { name: "Photos", path: "/photos", component: Photos }
  // { path: '/:catchAll(.*)', component: NotFound }
