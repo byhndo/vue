e738a8faaff05f87660129e10e91e1c698b09c7c
byhndo <120541041+byhndo@users.noreply.github.com>
2025-07-18 19:43:13 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -141,13 +141,47 @@ const { createRouter, createWebHistory } = VueRouter;
 const app = Vue.createApp({ 
 setup(){
 const bioPath = ref(null);
-    const photosPath = ref(null);
+const photosPath = ref(null);
 
-bgPath(bg);
-  return {
-   bioPath,
-   photosPath
+//const bioPath = document.getElementById("bioPath");
+//const photosPath = document.getElementById("photosPath");
+	
+    const paths = {
+    step1: {
+      unfilled: "M 0 0 h 0 c 0 50 0 50 0 100 H 0 V 0 Z",
+      inBetween: "M 0 0 h 33 c -30 54 113 65 0 100 H 0 V 0 Z",             
+      filled: "M 0 0 h 100 c 0 50 0 50 0 100 H 0 V 0 Z"
+    },
+
+    step2: {
+      unfilled: "M 100 0 h 0 c 0 50 0 50 0 100 H 100 V 0 Z",  
+      inBetween: "M 100 0 h -33 c 30 54 -113 65 0 100 H 100 V 0 Z",
+      filled: "M 100 0 h -100 c 0 50 0 50 0 100 H 100 V 0 Z"
+    }
   };
+
+ const tl1 = gsap.timeline({ paused: true })
+
+ .set(bioPath, {
+    attr: { d: paths.step1.unfilled }})
+    .to(bioPath,{duration: 1.1, ease: "power3.in", attr: { d: paths.step1.inBetween }}, 0)
+    .to(bioPath, {duration: .5, ease: "power1", attr: { d: paths.step1.filled }});
+
+  const tl2 = gsap.timeline({paused: true}) 
+
+    .set(photosPath, {attr: { d: paths.step2.unfilled }})
+    .to(photosPath, {duration: 1.1, ease: "power3.in", attr: { d: paths.step2.inBetween }}, 0)
+    .to(photosPath, {duration: .5, ease: "power1", attr: { d: paths.step2.filled }});  
+
+  const bioBtn = document.getElementById("btn-nav-1");
+  const photosBtn = document.getElementById("btn-nav-2"); 
+  
+  bioBtn.addEventListener("click", () => { tl1.restart(); });
+  photosBtn.addEventListener("click", () => { tl2.restart(); }); 
+	
+	
+
+return {bioPath, photosPath, paths, tl1, tl2, bioBtn, photosBtn };
 
 	
 },
@@ -442,44 +476,6 @@ gsap.to('.header', {
     }
 });
 
-function bgPath(bg) {
-const bioPath = document.getElementById("bioPath");
-const photosPath = document.getElementById("photosPath");
-	
-    const paths = {
-    step1: {
-      unfilled: "M 0 0 h 0 c 0 50 0 50 0 100 H 0 V 0 Z",
-      inBetween: "M 0 0 h 33 c -30 54 113 65 0 100 H 0 V 0 Z",             
-      filled: "M 0 0 h 100 c 0 50 0 50 0 100 H 0 V 0 Z"
-    },
-
-    step2: {
-      unfilled: "M 100 0 h 0 c 0 50 0 50 0 100 H 100 V 0 Z",  
-      inBetween: "M 100 0 h -33 c 30 54 -113 65 0 100 H 100 V 0 Z",
-      filled: "M 100 0 h -100 c 0 50 0 50 0 100 H 100 V 0 Z"
-    }
-  };
-
- const tl1 = gsap.timeline({ paused: true })
-
- .set(bioPath, {
-    attr: { d: paths.step1.unfilled }})
-    .to(bioPath,{duration: 1.1, ease: "power3.in", attr: { d: paths.step1.inBetween }}, 0)
-    .to(bioPath, {duration: .5, ease: "power1", attr: { d: paths.step1.filled }});
-
-  const tl2 = gsap.timeline({paused: true}) 
-
-    .set(photosPath, {attr: { d: paths.step2.unfilled }})
-    .to(photosPath, {duration: 1.1, ease: "power3.in", attr: { d: paths.step2.inBetween }}, 0)
-    .to(photosPath, {duration: .5, ease: "power1", attr: { d: paths.step2.filled }});  
-
-  const bioBtn = document.getElementById("btn-nav-1");
-  const photosBtn = document.getElementById("btn-nav-2"); 
-  
-  bioBtn.addEventListener("click", () => { tl1.restart(); });
-  photosBtn.addEventListener("click", () => { tl2.restart(); }); 
-	
-} 
 		
 function setupReveal(container) {
 container.ctx = gsap.context(() => {	
