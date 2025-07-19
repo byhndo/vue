8e58e980474e625b8fce64a6fb8df065e0db4a5d
byhndo <120541041+byhndo@users.noreply.github.com>
2025-07-19 20:52:34 +0700

    Update router.js

--- js/router.js
+++ js/router.js
@@ -2,11 +2,10 @@ const routes = [
   { path: '/', redirect: '/bio'},
   { name: "Bio", path: "/bio", component: Bio },
   { name: "Photos", path: "/photos", component: Photos }
-//  { path: '/:catchAll(.*)', component: NotFound }
 ];
 
 const router = VueRouter.createRouter({
-  history: VueRouter.createWebHistory('/vue-ghpages-cdn/'),
+  history: VueRouter.createWebHistory('/vue/'),
   routes,
   scrollBehavior(to, from, savedPosition) {
     if (savedPosition) {
